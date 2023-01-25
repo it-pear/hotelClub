e2960429203412b7c3629b46e90dc49a47da9386
itpear <yurecblinovgelarm@gmail.com>
2023-01-25 06:05:58 +0300

    главный экран

---
+++ src/components/pages/home/GeneralPage.vue
@@ -0,0 +1,186 @@
+<template>
+  <section class="general-page">
+    <q-dialog
+      v-model="accept"
+    >
+      <q-card style="width: 700px; max-width: 80vw;">
+        <q-card-section>
+          <div class="text-h6">Medium</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Click/Tap on the backdrop.
+        </q-card-section>
+
+        <q-card-actions align="right" class="bg-white text-teal">
+          <q-btn flat label="OK" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    
+    <div class="column container">
+      <h1 class="text-h1 text-weight-medium text-white">Недвижимость в Турции</h1>
+      <h3 class="text-h4 text-white">Покупка, Аренда недвижимости в Турции от звонка до покупки</h3>
+    </div>
+    <div class="column container">
+      <q-form
+        @submit="onSubmit"
+        @reset="onReset"
+        class="q-gutter-md"
+      >
+        <q-tabs
+          v-model="tab"
+          inline-label
+          class="bg-blue-grey-1 text-blue-grey-13 shadow-2"
+        >
+          <q-tab name="1" icon="home" label="Покупка" />
+          <q-tab name="2" icon="money" label="Аренда" />
+        </q-tabs>
+        <div class="row content bg-blue-grey-1">
+          <div class="col-3 q-pa-sm">
+            <q-input
+              type="number"
+              v-model="form.id"
+              label="ID Объекта"
+              lazy-rules
+            />
+          </div>
+          <div class="col-3 q-pa-sm">
+            <q-select
+              v-model="model"
+              :options="options"
+              label="Количество комнат"
+              multiple
+              emit-value
+              map-options
+            >
+              <template v-slot:option="{ itemProps, opt, selected, toggleOption }">
+                <q-item v-bind="itemProps">
+                  <q-item-section>
+                    <q-item-label v-html="opt.label" />
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-toggle :model-value="selected" @update:model-value="toggleOption(opt)" />
+                  </q-item-section>
+                </q-item>
+              </template>
+            </q-select>
+          </div>
+          <div class="col-3 q-pa-sm">
+            <q-input
+              type="number"
+              v-model="form.priceTo"
+              label="Цена От"
+              lazy-rules
+              :rules="[
+                val => val !== null && val !== '' || 'Пожалуйста заполните это поле',
+              ]"
+            />
+          </div>
+          <div class="col-3 q-pa-sm">
+            <q-input
+              type="number"
+              v-model="form.PriceFor"
+              label="Цена до"
+              lazy-rules
+              :rules="[
+                val => val !== null && val !== '' || 'Пожалуйста заполните это поле',
+              ]"
+            />
+          </div>
+
+          <div class="row col-12">
+            <q-toggle 
+              v-model="accept" 
+              label="Дополнительные параметры" 
+            />
+            <q-btn label="Поиск" unelevated rounded type="submit" color="primary" class="q-ml-auto"/>
+            <q-btn label="Сбросить" type="reset" color="primary" flat class="q-ml-sm" />
+          </div>
+
+        </div>
+        
+      </q-form>
+    </div>
+  </section>
+</template>
+
+<script>
+import { useQuasar } from 'quasar'
+import {ref} from 'vue'
+
+export default {
+  setup() {
+    const $q = useQuasar()
+
+    const form = ref({
+      id: '',
+      priceTo: '',
+      PriceFor: '',
+    })
+    const accept = ref(false)
+
+    const model = ref(null)
+    const options = ref([
+      {
+        label: '1 км.',
+        value: 1
+      },
+      {
+        label: '2 км.',
+        value: 2
+      },
+      {
+        label: '3 км.',
+        value: 3
+      },
+      {
+        label: '4 км.',
+        value: 4
+      },
+      {
+        label: '5 км.',
+        value: 5
+      },
+      {
+        label: '6 км.',
+        value: 6
+      },
+    ])
+    
+
+    return {
+      form,
+      accept,
+      tab: ref('1'),
+      model,
+      options,
+
+      onSubmit () {
+        if (accept.value !== true) {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'You need to accept the license and terms first'
+          })
+        }
+        else {
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'Submitted'
+          })
+        }
+      },
+
+      onReset () {
+        name.value = null
+        age.value = null
+        accept.value = false
+      }
+    }
+  },
+}
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,116 +1,113 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <q-layout view="hhh LpR fff">
+    
+    <q-header height-hint="98" style="border-bottom: 1px solid #eceff1;">
+      <q-toolbar class="toolbar-info bg-blue-grey-1">
+          <div class="row container">
+            <div class="col">
+              <a href="tel:89999999999" class="phone text-body2 text-weight-medium text-blue-grey-13">8 (999) 999 99-99</a>
+              <a href="mailto:example@gmail.com" class="phone text-body2 text-weight-medium text-blue-grey-13 q-ml-lg">example@gmail.com</a>
+            </div>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+            <div class="col flex justify-end">
+              <q-btn flat round color="primary" icon="telegram" />
+              <q-btn flat round color="primary" icon="telegram" />
+              <q-btn flat round color="primary" icon="telegram" />
+            </div>
+          </div>
+          
+        <!-- <q-btn icon="telegram" /> -->
       </q-toolbar>
+      <q-toolbar class="bg-white text-blue-grey-13" color="blue-grey-13" style="min-height: 80px;">
+        <!-- <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" /> -->
+          <div class="container">
+            <q-toolbar-title>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
+              </q-avatar>
+            </q-toolbar-title>
+
+            <q-tabs align="left" class="tabs-link">
+              <q-route-tab
+                v-for="link in routes"
+                :key="link"
+                :to="link.link"
+                :label="link.name"
+              />
+            </q-tabs>
+            <q-btn unelevated rounded color="primary" label="Войти" class="q-ml-auto" />
+          </div>
+        <!-- <q-btn dense flat round icon="menu" @click="toggleRightDrawer" /> -->
+      </q-toolbar>
+
+      
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+    <q-drawer v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+    <q-drawer v-model="rightDrawerOpen" side="right" bordered>
+      <!-- drawer content -->
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref } from 'vue'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
-export default defineComponent({
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
+export default {
   setup () {
     const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
+
+    const routes = ref([
+      {
+        name: 'Каталог недвижимости',
+        link: ''
+      },
+      {
+        name: 'Оформление документов',
+        link: ''
+      },
+      {
+        name: 'О нас',
+        link: ''
+      },
+      {
+        name: 'Контакты',
+        link: ''
+      },
+    ])
 
     return {
-      essentialLinks: linksList,
+      routes,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
       }
     }
   }
-})
+}
 </script>
